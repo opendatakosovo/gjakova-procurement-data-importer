8a468bd7fb19c202be280673a2fc9a65e6ee6874
Taulant Ukmata <taulantukmata@gmail.com>
2014-10-10 15:20:12 +0200

    Created the definition for removing commas to change total from string to float

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import csv
 from pymongo import MongoClient
 from bson import ObjectId
 
+
 # Connect to default local instance of mongo
 client = MongoClient()
 
@@ -10,37 +11,37 @@ client = MongoClient()
 db = client.gjakovadata
 collection = db.procurement2010
 
+
 def result():
 
-	with open('Gjakova_2011.csv', 'rb') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',')
-		for row in reader:
-			group = row[0]
-			forecast = row[1]
-			commune = row[2]
-			type1 = row[3]
-			type2 = row[4]
-			type3 = row[5]
-			total = row[8]
-			date = row[9]
-		
-			#print grupi + parashikimi + komuna + tipi1 + tipi2 + tipi3 + shuma+ data
-			
-			doc = {
-				"group":group,
-				"forecast":forecast,
-				"commune":commune,
-				"type1":type1,
-				"type2":type2,
-				"type3":type3,
-				"total":total,
-				"date":date
-				
-			}
-			print doc
-			collection.insert(doc)
+    with open('Gjakova_2011.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            group = row[0]
+            forecast = row[1]
+            commune = row[2]
+            type1 = row[3]
+            type2 = row[4]
+            type3 = row[5]
+            total = remove_comma(row[8])
+            date = row[9]
+
+            doc = {
+                "group": group,
+                "forecast": forecast,
+                "commune": commune,
+                "type1": type1,
+                "type2": type2,
+                "type3": type3,
+                "total": float(total),
+                "date": date
+            }
+
+            collection.insert(doc)
+
+
+def remove_comma(number):
+    return number.replace(",", "")
+
 
 result()
-
-
-
