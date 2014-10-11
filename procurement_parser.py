9545dfec90aeac3234742caf798609a53e776048
Your Name <partin.imeri@gmail.com>
2014-10-11 02:39:14 +0200

    added functions to get proper values for fields in importer

--- procurement_parser.py
+++ procurement_parser.py
@@ -27,9 +27,9 @@ def parse():
                     year = int(filename.replace('.csv', ''))
                     budget_type = get_buget_type(row[0])
                     nr = row[1]
-                    type_of_procurement = int(row[2])
-                    value_of_procurement = int(row[3])
-                    procurement_procedure = int(row[4])
+                    type_of_procurement = get_procurement_type(int(row[2]))
+                    value_of_procurement = get_procurement_value(int(row[3]))
+                    procurement_procedure = get_procurement_procedure(int(row[4]))
                     classification = int(row[5])
                     activity_title_of_procurement = row[6]
                     signed_date = row[7] #TODO: Convert this to Date
@@ -38,28 +38,32 @@ def parse():
                     aneks_contract_price = row[10]
                     company = row[11]
                     company_address = row[12]
-                    #due_contract_accepting_time = int(row[13])
-                    #contract_criteria = int(row[14])
+                    tipi_operatorit = get_company_type(row[13])
+                    afati_kohor = get_due_time(row[14])
+                    kriteret_per_dhenje_te_kontrates = get_criteria_type(row[15])
 
                     report = {
-                        "year": year,
-                        "budgetType": budget_type,
-                        "number": nr,
-                        "type": type_of_procurement,
-                        "value": value_of_procurement,
-                        "procedure": procurement_procedure,
-                        "classification": classification,
-                        "activity": activity_title_of_procurement,
-                        "signedDate": signed_date,
-                        "contract": {
-                            "value": contract_value,
-                            "price": contract_price,
-                            "aneksPrice": aneks_contract_price,
+                        "viti": year,
+                        "tipiBugjetit": budget_type,
+                        "numri": nr,
+                        "tipi": type_of_procurement,
+                        "vlera": value_of_procurement,
+                        "procedura": procurement_procedure,
+                        "klasifikimi": classification,
+                        "aktiviteti": activity_title_of_procurement,
+                        "dataNenshkrimit": signed_date,
+                        "kontrata": {
+                            "vlera": contract_value,
+                            "qmimi": contract_price,
+                            "qmimiAneks": aneks_contract_price,
+                            "afatiKohor": afati_kohor,
+                            "kriteret": kriteret_per_dhenje_te_kontrates
                         },
-                        "company": {
-                            "name": company,
+                        "kompania": {
+                            "emri": company,
                             "slug": "",
-                            "address": company_address,
+                            "selia": company_address,
+                            "tipiKompanise": tipi_operatorit
                         }
                     }
 
@@ -70,13 +74,101 @@ def parse():
 
 
 def get_buget_type(number):
-    if number == 1:
-        return "Te hyrat vetanake"
-    elif number == 2:
-        return "Buxheti i Konsoliduar i Kosoves"
-    elif number == 3:
-        return "Donacion"
+    if number != "":
+        num = int(number)
+        if num == 1:
+            return "Te hyrat vetanake"
+        elif num == 2:
+            return "Buxheti i Konsoliduar i Kosoves"
+        elif num == 3:
+            return "Donacion"
+        else:
+            return ""
     else:
-        ""
+        return ""
+
+
+def get_procurement_type(number):
+    if number == 1:
+        return "Furnizim"
+    elif number == 2:
+        return "Sherbime"
+    elif number == 3:
+        return "Sherbime Keshillimi"
+    elif number == 4:
+        return "Konkurs projektimi"
+    elif number == 5:
+        return "Pune"
+    elif number == 6:
+        return "Pune me koncesion"
+    elif number == 7:
+        return "Prone e palujtshme"
+    else:
+        return ""
+
+
+def get_procurement_value(number):
+    if number == 1:
+        return "Vlere e madhe"
+    elif number == 2:
+        return "Vlere e mesme"
+    elif number == 3:
+        return "Vlere e vogel"
+    elif number == 4:
+        return "Vlere  minimale"
+    else:
+        return ""
+
+
+def get_procurement_procedure(number):
+    if number == 1:
+        return "Procedura e hapur"
+    elif number == 2:
+        return "Procedura e kufizuar"
+    elif number == 3:
+        return "Konkurs projektimi"
+    elif number == 4:
+        return "Procedura e negociuar pas publikimit te njoftimit te kontrates"
+    elif number == 5:
+        return "Procedura e negociuar pa publikimit te njoftimit te kontrates"
+    elif number == 6:
+        return "Procedura e kuotimit te Cmimeve"
+    elif number == 7:
+        return "Procedura e vleres minimale"
+    else:
+        return ""
+
+
+def get_company_type(num):
+    if num != "":
+        number = int(num)
+        if number == 1:
+            return "OE Vendor"
+        elif number == 2:
+            return "OE Jo vendor"
+    else:
+        return ""
+
+
+def get_due_time(num):
+    if num != "":
+        number = int(num)
+        if number == 1:
+            return "Afati kohor normal"
+        elif number == 2:
+            return "Afati kohor i shkurtuar"
+    else:
+        return ""
+
+
+def get_criteria_type(num):
+    if num != "":
+        number = int(num)
+        if number == 1:
+            return "Cmimi me i ulet"
+        elif number == 2:
+            return "Tenderi ekonomikisht me i favorshem"
+    else:
+        return ""
 
 parse()
